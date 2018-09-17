39d68e0ac8f5d77b1da688ea184e0e8d02d07d0f
Villiam Von Doom <jinglongh@yahoo.com>
2018-09-18 01:27:01 +1000

    Add user and manage user front end done

--- web/script.js
+++ web/script.js
@@ -4,15 +4,6 @@
  * and open the template in the editor.
  */
 $(document).ready(function(){
-  $('.header').load("header.html");
-  
-    // Get the modal
-    var modal = document.getElementById('loginForm');
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-     } 
+    $('.header').load("header.html");
+    
 });
